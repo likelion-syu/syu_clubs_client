96ea8f60c7518f6d1e6ba0b9bcd43d081af4bb5d
sneakstarberry <gihong2012@likelion.org>
2020-03-20 13:33:10 +0900

    서버 연결 확인 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+async function getClubs() {
+  const response = await axios.get<Clubs>(
+    'http://127.0.0.1:8000/api/union/club/'
+  );
+  return response.data;
+}
+
+interface Clubs {
+  club_id: number;
+  club_name: string;
+  club_desc: string;
+  user: object;
+  club_img_url: string;
+  created_at: Date;
+  updated_at: Date;
+  is_central: string;
+  is_united: string;
+  club_type: object;
+}
 
 function App() {
+  console.log(getClubs());
   return (
     <div className="App">
       <header className="App-header">
